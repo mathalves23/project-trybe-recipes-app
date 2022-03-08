7aa78b3f8c577ee32c3d4ed38ced45aee20dacda
CassidHorimi <cassid@outlook.com.br>
2022-03-08 16:54:45 -0300

    cria a tela de explorar foods e drinks

--- src/Pages/ExploreFoods.js
+++ src/Pages/ExploreFoods.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function ExploreFoods() {
+  const history = useHistory();
   return (
     <div>
       <Header route="Explore Foods" hasSearch={ false } />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => {
+          history.push('/explore/foods/ingredients');
+        } }
+      >
+        By Ingredient
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-nationality"
+        onClick={ () => {
+          history.push('/explore/foods/nationalities');
+        } }
+      >
+        By Nationality
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => {
+          history.push('/explore/foods/${}');
+        } }
+      >
+        Surprise me!
+      </button>
+      <br />
       Aqui ficará o conteúdo da página Explore Foods.
       <Footer />
     </div>
