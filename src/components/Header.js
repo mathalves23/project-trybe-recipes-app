c9c5ea8b4548f563dd842245a5beed9adb02511e
Matheus de Araújo Alves <matheus.aalves@MacBook-Air-de-Matheus.local>
2022-03-08 15:20:08 -0300

    Adiciona barra de pesquisa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 export default function Header({ route, hasSearch }) {
+  const [showSearchBar, setShowSearchBar] = useState(false);
   return (
     <header>
       <Link to="/profile">
         <img data-testid="profile-top-btn" alt="profile link" src={ profileIcon } />
       </Link>
       <h1 data-testid="page-title">{ route }</h1>
-      {
-        hasSearch
-          ? <img data-testid="search-top-btn" alt="search button" src={ searchIcon } />
-          : null
-      }
+      { hasSearch ? (
+        <button
+          type="button"
+          onClick={ () => setShowSearchBar(!showSearchBar) }
+        >
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="explore"
+          />
+        </button>) : null}
+      <span>
+        { showSearchBar ? <SearchBar /> : null }
+      </span>
     </header>
   );
 }
