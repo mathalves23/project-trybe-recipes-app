830071a3359f66016a2d33ae01ef45073c6278ba
CassidHorimi <cassid@outlook.com.br>
2022-03-10 15:02:34 -0300

    tela de explorar por ingredients

--- src/Pages/ExploreDrinksIngredients.js
+++ src/Pages/ExploreDrinksIngredients.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import MyContext from '../context/Context';
 
 function ExploreDrinksIngredients() {
+  const {
+    ingredientsDrink,
+    getDrinkIngredient,
+  } = useContext(MyContext);
+
+  const MAX_LENGTH = 12;
+
+  useEffect(() => {
+    getDrinkIngredient();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function renderLengthValidation(params) {
+    if (params !== undefined) {
+      return params.map((ingredient, index) => {
+        if (index < MAX_LENGTH) {
+          return (
+            <div
+              data-testid={ `${index}-ingredient-card` }
+            >
+              <img
+                src={ `https://www.thecocktaildb.com/images/ingredients/${ingredient.strIngredient1}-Small.png` }
+                alt={ ingredient.strIngredient1 }
+                data-testid={ `${index}-card-img` }
+              />
+              <p
+                data-testid={ `${index}-card-name` }
+              >
+                { ingredient.strIngredient1 }
+              </p>
+            </div>
+          );
+        }
+        return null;
+      });
+    }
+    return (<p>Nada encontrado.</p>);
+  }
+
   return (
     <div>
       <Header route="Explore Ingredients" hasSearch={ false } />
-      Aqui ficará o conteúdo da página Explore Drinks Ingredients.
+      { renderLengthValidation(ingredientsDrink) }
       <Footer />
     </div>
   );
--- src/Pages/ExploreFoodsIngredients.js
+++ src/Pages/ExploreFoodsIngredients.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import MyContext from '../context/Context';
 
 function ExploreFoodsIngredients() {
+  const {
+    ingredientsFood,
+    getFoodIngredient,
+  } = useContext(MyContext);
+
+  const MAX_LENGTH = 12;
+
+  function renderLengthValidation(params) {
+    if (params !== undefined) {
+      return params.map((ingredient, index) => {
+        if (index < MAX_LENGTH) {
+          return (
+            <div
+              data-testid={ `${index}-ingredient-card` }
+            >
+              <img
+                src={ `https://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png` }
+                alt={ ingredient.strIngredient }
+                data-testid={ `${index}-card-img` }
+              />
+              <p
+                data-testid={ `${index}-card-name` }
+              >
+                { ingredient.strIngredient }
+              </p>
+            </div>
+          );
+        }
+        return null;
+      });
+    }
+    return (<p>Nada encontrado.</p>);
+  }
+
+  useEffect(() => {
+    getFoodIngredient();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <Header route="Explore Ingredients" hasSearch={ false } />
-      Aqui ficará o conteúdo da página Explore Foods Ingredients.
+      { renderLengthValidation(ingredientsFood) }
       <Footer />
     </div>
   );
